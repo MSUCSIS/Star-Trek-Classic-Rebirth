9aeba453534d19e7f0d45b5899c664ea6d8b6c07
BurgerDan <dnlyoung15@gmail.com>
2017-02-21 17:14:05 -0600

    Add files via upload

--- PhaserShoot.cs
+++ PhaserShoot.cs
@@ -3,46 +3,61 @@ using System.Collections;
 
 public class PhaserShoot : MonoBehaviour
 {
-    
-    public Transform Phasers1;
-    public Transform target;
-    public LineRenderer lineRenderer;
+    //Found on Unity Website
+    //https://unity3d.com/learn/tutorials/projects/lets-try-assignments/lets-try-shooting-raycasts-article?playlist=41639
 
-    //DPS = Damage per shot, TBS = time between shot
-    public int DPS = 10;
-    float TBS = 1.0f;
-    public float range = 100f;
-    
-    
-    public Transform beamHitFXPrefab;
+    public int DPS = 1;
+    public float fireRate = 1.0f;
+    public float wpnRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
 
-    //initilization
-    void start()
-        {
-               
-        }
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
 
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+
+        gunAudio = GetComponent<AudioSource>();
+
+        fpsCam = GetComponentInParent<Camera>();
+    }
     void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
-            /*if(target == null)
+            nextFire = Time.time + fireRate;
+            StartCoroutine(ShotEffect());
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+            RaycastHit hit;
+            laserLine.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, wpnRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+                EnemyHealth health = hit.collider.GetComponent<EnemyHealth>();
+
+                if(health != null)
                 {
-                    lineRenderer.enabled = false;
-                    return;
-                }*/
-        lineRenderer.enabled = true;
-
-        /*if (!IsWithinRange())
-            return;
-            */
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * wpnRange));
+            }
         }
+    }
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
 
-    /* void Update()
-         {
-         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-         RaycastHit hit;
-
-         if (Physics.Raycast(ray, out hit, 100))
-             Debug.DrawLine(ray.origin, hit.point);
-         }
-         */
 }
